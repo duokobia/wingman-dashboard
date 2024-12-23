cb08b72693c01e8f02c411cf0a7522c4bc7ce320
Daniel Okobia <87054930+duokobia@users.noreply.github.com>
2024-12-23 23:16:32 +0100

    Initial Scaffolding

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-green-400">
+      <h1>Wingman Dashboard</h1>
+    </div>
+  );
+}
+
+export default App;
