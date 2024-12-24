47b90e636d8fe414198cb715785bc593fb0ae169
Daniel Okobia <87054930+duokobia@users.noreply.github.com>
2024-12-24 10:04:06 +0100

    style: Style header component

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'wingman-blue': '#1DA1F2', 
+        'wingman-green': '#115E56',
+        'wingman-teal': '#CCFBEF',
+        'wingman-turquiose': '#15879F',
+        'wingman-red': '#F04438',
+        'wingman-yellow': '#FFF3C6',
+        'wingman-black': '#212636',
+        'wingman-gray': '#667085',
+        'wingman-lightgray': '#8A94A6',
+      },
+    },
   },
   plugins: [],
 }
